0f0687c558ba0bfc2a2596efcf38fbefbbc81bbf
Sergio Durand <sergiodurand@gmail.com>
2018-10-23 21:34:05 -0300

    Initial commit

---
+++ jasper-examples/src/main/java/com/bkpmind/reports/jasper_examples/App.java
@@ -0,0 +1,13 @@
+package com.bkpmind.reports.jasper_examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
